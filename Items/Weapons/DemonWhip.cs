4033648bcbbfde9e4c41757871986e1e80c4bc6e
Stopstealingmyname <50157461+Stopstealingmyname@users.noreply.github.com>
2020-12-11 13:30:20 +0100

    Add files via upload

---
+++ Items/Weapons/DemonWhip.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MSB.Items.Weapons
+{
+    public class DemonWhip : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+
+            Tooltip.SetDefault("\n5 summon tag damage" + "\nYour summons will focus struck enemies");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 20;
+            item.knockBack = 8f;
+            item.crit = 9;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useAnimation = 40;
+            item.useTime = 30;
+            item.width = 30;
+            item.height = 40;
+            item.UseSound = SoundID.Item1;
+            item.noMelee = true;
+            item.summon = true;
+            item.noUseGraphic = true;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("DemonWhipProjectile");
+            item.shootSpeed = 4.5f;
+            item.value = 1000;
+
+        }
+        public override void HoldItem(Player player)
+        {
+            item.autoReuse = player.GetModPlayer<MSBPlayer>().WhipAutoswing;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DemoniteBar, 12);
+            recipe.AddIngredient(ItemID.Chain, 5);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
